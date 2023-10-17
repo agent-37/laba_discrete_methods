8d8394fbb88769928df4fccdf958969541679de4
agent-37 <95122754+agent-37@users.noreply.github.com>
2023-10-17 11:22:28 +0300

    Update 2_2.py
    Поменял код во время пары

--- 2_2.py
+++ 2_2.py
@@ -10,24 +10,38 @@ def dist(vec_a):
     return a.real
 
 
+def dist2(vec_a, vec_b):
+    sum = 0
+    for i in range(len(vec_a)):
+        sum = max(sum, abs(vec_a[i] - vec_b[i]))
+    return sum
+
+
 def read_matrix():
     str = input("Введите матрицу в таком формате. Пример формата. 1.7 0.8 0.9; 0.8 0.7 0.3; 0.9 0.3 1.7\n")
     return np.matrix(str)
 
+
 def read_vector():
     str = input("Введите матрицу в таком формате. Пример формата. 1.7; 0.3; 1.7\n")
     return np.matrix(str)
+
+
 def check_correct_matrix(matrix):
     for i in range(len(matrix)):
-        if matrix[i, i] == 0:
+        sum = 0
+        for j in range(len(matrix)):
+            sum += abs(matrix[i, j])
+        if sum >= abs(matrix[i, i]) == 0:
             print('Матрица не коректна')
-            return 1
-    return 0
+            return 0
+    return 1
+
 
 epsilon = 0.00001
 
 
-def find_x_n(matrix, cur,B, s):
+def find_x_n(matrix, cur, B, s):
     ans = np.matrix(s)
     for i in range(len(matrix)):
         sum = 0
@@ -35,7 +49,7 @@ def find_x_n(matrix, cur,B, s):
             sum += matrix[i, j] * ans[j]
         for j in range(i + 1, len(matrix)):
             sum += matrix[i, j] * cur[j]
-        ans[i] = (sum+B[i]) / matrix[i, i]
+        ans[i] = (sum + B[i]) / matrix[i, i]
     return ans
 
 
@@ -46,14 +60,18 @@ def method(matrix, epsilon, B):
     vector_x = [np.matrix(s)]
     for i in range(100):
         vector_x.append(find_x_n(matrix, vector_x[len(vector_x) - 1], B, s))
-        if dist(vector_x[len(vector_x) - 1] - vector_x[len(vector_x) - 2]) < epsilon:
+        if np.linalg.norm(matrix) / abs(1.0 - np.linalg.norm(matrix)) * dist2(
+                vector_x[len(vector_x) - 1], vector_x[len(vector_x) - 2]) < epsilon:
             print("Решение =", vector_x[len(vector_x) - 1], "полученный на шаге", i)
-            return
+            # print(vector_x[len(vector_x)-2])
+            # print(np.linalg.norm(matrix))
 
+            return
+    print('Для данной матрицы метод Зейделя не работает')
 
 matrix = read_matrix()
-B = read_vector()
-check_correct_matrix(matrix)
-method(matrix, epsilon,B)
+if check_correct_matrix(matrix):
+    B = read_vector()
+    method(matrix, epsilon, B)
 
-# 0, -0.1, 0.1; -0.1, 0, 0.1; 0.1, -0.1, 0
+# 2, -0.1, 0.1; -0.1, 2, 0.1; 0.1, -0.1, 2
