b86a8247e7662d8ea58644407271c40725f6662b
agent-37 <95122754+agent-37@users.noreply.github.com>
2023-10-31 11:47:02 +0300

    Update 2_2.py
    Изменил код на паре

--- 2_2.py
+++ 2_2.py
@@ -17,6 +17,12 @@ def norm_vec(vec_a):
         buf_max += vec_a[i] * vec_a[i]
     return vec_a / sqrt(buf_max).real
 
+def norm_max(vec_a):
+    '''Функция для нормализации вектора'''
+    buf_max = 0
+    for i in range(len(vec_a)):
+        buf_max =max(buf_max, abs(vec_a[i]))
+    return buf_max
 
 def dist2(vec_a, vec_b):
     sum = 0
@@ -65,17 +71,18 @@ def check_correct_matrix(matrix):
 
     count_poly(0)
     max1 = 0
+    print(polinom)
     r = np.roots(polinom)
+    print(np.roots(polinom))
     for i in range(len(r)):
         max1 = max(max1, abs(r[i]).real)
-    if max1 < 1:
+    if max1 < 0.8:
         return 1
     else:
-        print("Метод Зейделя не пременим")
         return 0
 
 
-epsilon = 0.00001
+epsilon = 0.0001
 
 
 def find_x_n(matrix, cur, B, s):
@@ -93,14 +100,16 @@ def find_x_n(matrix, cur, B, s):
 def method(matrix, epsilon, B):
     s = '1.0'
     for i in range(len(matrix) - 1):
-        s = s + '; 0.0'
+        s = s + '; 1.0'
     vector_x = [np.matrix(s)]
-    for i in range(1000):
+    for i in range(10000):
+        # print(find_x_n(matrix, vector_x[len(vector_x) - 1], B, s))
         vector_x.append(find_x_n(matrix, vector_x[len(vector_x) - 1], B, s))
-        if np.linalg.norm(matrix) / abs(1.0 - np.linalg.norm(matrix)) * dist2(
-                vector_x[len(vector_x) - 1], vector_x[len(vector_x) - 2]) < epsilon:
 
+        if (norm_max(vector_x[len(vector_x) - 1] - vector_x[len(vector_x) - 2])) / (
+        norm_max(vector_x[len(vector_x) - 2])) < epsilon:
             print("Решение =", vector_x[len(vector_x) - 1], "полученный на шаге", i)
+            print("Невязка", matrix @ vector_x[len(vector_x)-1]-B)
             # print(vector_x[len(vector_x)-2])
             # print(np.linalg.norm(matrix))
 
@@ -110,7 +119,27 @@ def method(matrix, epsilon, B):
 
 matrix = read_matrix()
 B = read_vector()
+#check_correct_matrix(matrix)
+#B = matrix.transpose() @ B
+#matrix = matrix.transpose() @ matrix
 if (check_correct_matrix(matrix)):
     method(matrix, epsilon, B)
-
+else:
+    B = matrix.transpose() @ B
+    matrix = matrix.transpose() @ matrix
+    if (check_correct_matrix(matrix)):
+        method(matrix, epsilon, B)
+    else:
+        print('Приводите к диагональному виду')
+        B = np.linalg.inv(matrix) @ B
+        matrix = np.linalg.inv(matrix) @  matrix
+        print(B)
 # 2, -0.1, 0.1; -0.1, 2, 0.1; 0.1, -0.1, 2
+
+
+#  7.6 5.8 4.7; 3.8 4.1 2.7; 2.9 2.1 3.8
+# 10.1; 9.7; 7.8
+
+
+# 1.7 2.8 1.9; 2.1 3.4 1.8; 4.2 -1.7 1.3
+# 0.7;1.1; 2.8
