93dede899c8fdaa4ad7010b8291dbb527e5e9d9d
Иван Мслеников <bdfy.maslenikov@yandex.ru>
2024-02-29 14:19:42 +0300

    test push

--- lab_4.py
+++ lab_4.py
@@ -1,53 +1,73 @@
 from cmath import cos, sin
-from sympy import sin, cos, Matrix, diff, solve, N
+from sympy import sin, cos, Matrix, diff, solve, N, integrate
 import numpy as np
 from sympy.abc import x, y, t
+import scipy
 
 eps = 1e-5
 
 
 def func():
-    return np.e ** (-x)
+    return cos(t + x ** 2 + 0.5) / (2 + sin(x ** 2 + 1))
 
 
-def f(num):
-    return func().limit(x, num)
+def f(x, t):
+    return cos(t + x ** 2 + 0.5) / (2 + sin(x ** 2 + 1))
 
 
-def find_square_rectangle(a, b, N):
+def f_t(t1):
+    return func().limit(t, t1)
+
+
+def find_square_rectangle(a, b, t, N):
     h = (b - a) / N
     sum = 0
     for i in range(N):
-        sum += h * f(a + (i + 1 / 2) * h)
+        sum += h * f(a + (i + 1 / 2) * h, t)
     return sum
 
 
-def find_square_trapezoid(a, b, N):
+def find_square_trapezoid(a, b, t, N):
     h = (b - a) / N
     sum = 0
     for i in range(N):
-        sum += h * (f(a + i * h) + f(a + (i + 1) * h)) / 2
+        sum += h * (f(a + i * h, t) + f(a + (i + 1) * h, t)) / 2
     return sum
 
 
-def find_square_simpson(a, b, N):
+def find_square_simpson(a, b, t, N):
     h = (b - a) / N
     sum = 0
     for i in range(N):
-        sum += h * (f(a + i * h) + 4 * f(a + (i + 1 / 2) * h) + f(a + (i + 1) * h)) / 6
+        sum += h * (f(a + i * h, t) + 4 * f(a + (i + 1 / 2) * h, t) + f(a + (i + 1) * h, t)) / 6
     return sum
 
 
-def find_square(a, b, g):
+def find_square(a, b, t, g):
     N = 4
-    buf = g(a, b, N)
-    while (abs(buf - g(a, b, 2 * N)) > eps):
-        buf = g(a, b, 2 * N)
+    buf = g(a, b, t, N)
+    while abs(buf - g(a, b, t, 2 * N)) > eps:
+        buf = g(a, b, t, 2 * N)
         N *= 2
-    print(buf, g(a, b, 2 * N), N)
+    print(buf, N, end=' |')
 
 
+t1 = 0
+
+c, d, Nt = map(float, input('Введите отрезок [c,d] для времени t и количество шагов\n').split())
 a, b = map(float, input('Введите отрезок [a,b]\n').split())
-find_square(a, b, find_square_rectangle)
-find_square(a, b, find_square_trapezoid)
-find_square(a, b, find_square_simpson)
+print('Прямоугольники', 'Трапеции', 'Симпсон', sep='               ')
+for i in range(round(Nt)):
+    t1 = c + (d - c) / Nt * i
+    print(t1, end=' |')
+    find_square(a, b, t1, find_square_rectangle)
+    find_square(a, b, t1, find_square_trapezoid)
+    find_square(a, b, t1, find_square_simpson)
+
+
+    def fx(x):
+        global t1
+        return cos(t1 + x ** 2 + 0.5) / (2 + sin(x ** 2 + 1))
+
+
+    print(scipy.integrate.quad(fx, a, b))
