478894c7d14030f117cb1d054099d5da018321b6
Иван Мслеников <bdfy.maslenikov@yandex.ru>
2024-03-03 20:46:26 +0300

    test push

--- lab_5_Euler.py
+++ lab_5_Euler.py
@@ -1,3 +1,5 @@
+from cmath import e
+
 import matplotlib.pyplot as plt
 
 
@@ -13,14 +15,26 @@ def sys_f_y(t, x, y):
     return -y / t - x
 
 
+def f_e(t):
+    return e ** (t ** 2 / 2).real
+
+
 def Euler(t_0, x_0, a, b, h):
     points_t, points_x = [t_0], [x_0]
     while t_0 < b:
-        t_0 += h
         x_0 += h * f(t_0, x_0)
         points_t.append(t_0)
         points_x.append(x_0)
+        t_0 += h
     plt.plot(points_t, points_x)
+    pe_t, pe_x = [], []
+    dh = (b - a) / 1000
+    for i in range(1000):
+        pe_t.append(a + dh * i)
+        pe_x.append(f_e(a + dh * i))
+
+    plt.plot(pe_t, pe_x)
+
     plt.show()
 
 
@@ -36,10 +50,10 @@ def Euler_sys(t_0, x_0, y_0, a, b, h):
     plt.show()
 
 
-# a,b=0,1
-# t_0, x_0, h = map(float, input().split())
-# Euler(t_0, x_0, a, b, h)
-
-a, b = 1, 12
-t_0, x_0, y_0, h = 1, 0.7, -0.4, 0.1
-Euler_sys(t_0, x_0, y_0, a, b, h)
+a, b = 0, 1
+t_0, x_0, h = 0, 1, 0.1
+Euler(t_0, x_0, a, b, h)
+#
+# a, b = 1, 12
+# t_0, x_0, y_0, h = 1, 0.7, -0.4, 0.1
+# Euler_sys(t_0, x_0, y_0, a, b, h)
