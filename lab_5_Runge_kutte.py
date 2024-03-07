8dc7e6192a71855159993c51b4ff45fd658d8d20
Иван Мслеников <bdfy.maslenikov@yandex.ru>
2024-03-07 18:50:03 +0300

    test push

--- lab_5_Runge_kutte.py
+++ lab_5_Runge_kutte.py
@@ -8,11 +8,13 @@ def f(t, x):
 
 
 def sys_f_x(t, x, y):
-    return y
+    # return y
+    return 2 + y - x ** 2
 
 
 def sys_f_y(t, x, y):
-    return -y / t - x
+    # return -y / t - x
+    return 2 * x * (x - y)
 
 
 def f_e(t):
@@ -23,9 +25,9 @@ def R_K_2(t_0, x_0, a, b, h):
     points_t, points_x, pe_x = [t_0], [x_0], [x_0]
     while t_0 < b:
         x_0 += h * f(t_0 + h / 2, x_0 + h / 2 * f(t_0, x_0))
-        points_t.append(t_0+h)
+        points_t.append(t_0 + h)
         points_x.append(x_0)
-        pe_x.append(f_e(t_0+h))
+        pe_x.append(f_e(t_0 + h))
         t_0 += h
     plt.plot(points_t, points_x)
     plt.plot(points_t, pe_x)
@@ -33,6 +35,7 @@ def R_K_2(t_0, x_0, a, b, h):
     for i in range(len(points_t)):
         print(points_t[i], points_x[i], pe_x[i], abs(points_x[i] - pe_x[i]))
     plt.show()
+
 
 def R_K_4(t_0, x_0, a, b, h):
     points_t, points_x, pe_x = [t_0], [x_0], [x_0]
@@ -42,9 +45,9 @@ def R_K_4(t_0, x_0, a, b, h):
         k3 = h * f(t_0 + h / 2, x_0 + k2 / 2)
         k4 = h * f(t_0 + h, x_0 + k3)
         x_0 += 1 / 6 * (k1 + 2 * k2 + 2 * k3 + k4)
-        points_t.append(t_0+h)
+        points_t.append(t_0 + h)
         points_x.append(x_0)
-        pe_x.append(f_e(t_0+h))
+        pe_x.append(f_e(t_0 + h))
         t_0 += h
     plt.plot(points_t, points_x)
     plt.plot(points_t, pe_x)
@@ -53,10 +56,10 @@ def R_K_4(t_0, x_0, a, b, h):
         print(points_t[i], points_x[i], pe_x[i], abs(points_x[i] - pe_x[i]))
     plt.show()
 
+
 def R_K_4_S(t_0, x_0, y_0, a, b, h):
     points_x, points_y = [], []
-    t_0 = 0
-    h = 0.001
+    # h = 0.001
     while t_0 < b:
         k1 = h * sys_f_x(t_0, x_0, y_0)
         q1 = h * sys_f_y(t_0, x_0, y_0)
@@ -71,17 +74,38 @@ def R_K_4_S(t_0, x_0, y_0, a, b, h):
         points_x.append(x_0)
         points_y.append(y_0)
         t_0 += h
-    plt.plot(points_x,points_y)
+    plt.plot(points_x, points_y)
+    # plt.show()
 
 
 # a, b = 0, 1
 # t_0, x_0, h = 0, 1, 0.1
 # R_K_2(t_0, x_0, a, b, h)
 
-a, b = 0, 1
-t_0, x_0, h = 0, 1, 0.1
-R_K_4(t_0, x_0, a, b, h)
+# a, b = 0, 1
+# t_0, x_0, h = 0, 1, 0.1
+# R_K_4(t_0, x_0, a, b, h)
 #
 # a, b = 1, 12
 # t_0, x_0, y_0, h = 1, 0.7, -0.4, 0.1
 # R_K_4_S(t_0, x_0, y_0, a, b, h)
+
+# В варианте №8 из Матвеева для лабораторных работ, есть 3 точки равновесия (-1,-1),(0,-2),(2,2)
+t_0, h = 0, 0.01
+a, b = 0, 2
+eps=0.01
+R_K_4_S(t_0, -1-eps, -1, a, b, h)
+R_K_4_S(t_0, -1+eps, -1, a, b, h)
+R_K_4_S(t_0, -1, -1-eps, a, b, h)
+R_K_4_S(t_0, -1, -1+eps, a, b, h)
+
+R_K_4_S(t_0, 0-eps, -2, a, b, h)
+R_K_4_S(t_0, 0+eps, -2, a, b, h)
+R_K_4_S(t_0, 0, -2-eps, a, b, h)
+R_K_4_S(t_0, 0, -2+eps, a, b, h)
+
+R_K_4_S(t_0, 2-10*eps, 2, a, b, h)
+R_K_4_S(t_0, 2+10*eps, 2, a, b, h)
+R_K_4_S(t_0, 2, 2-10*eps, a, b, h)
+R_K_4_S(t_0, 2, 2+10*eps, a, b, h)
+plt.show()
